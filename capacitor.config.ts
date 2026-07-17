77427108248c448c144f9b51902e982953972cef
Lavaztech <lavaztech.net@gmail.com>
2026-07-17 10:49:55 -0400

    Initial Lava Prompt Lab project

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+// capacitor.config.ts
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.russell.lavaprompt',
+  appName: 'Lava Prompt Lab',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
